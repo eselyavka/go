57b4cc0baeeac056f729403d35889ad188c58be2
Evgenii Seliavka <eseliavka@twitter.com>
2022-08-02 10:46:33 -0400

    #249 add more tests + library function

--- localhost/leetcode/solutions_test.go
+++ localhost/leetcode/solutions_test.go
@@ -280,4 +280,16 @@ func TestSolution249(t *testing.T) {
 	res := groupStrings([]string{"abc", "bcd", "acef", "xyz", "az", "ba", "a", "z"})
 	expected := [][]string{{"a", "z"}, {"abc", "bcd", "xyz"}, {"az", "ba"}, {"acef"}}
 	assert.Equal(len(res), len(expected), "Solution249")
+
+	array_equals := make([]bool, 0)
+	for _, arr_res := range res {
+		exists := false
+		for _, arr_exp := range expected {
+			if stringSlicesEqual(arr_res, arr_exp) {
+				exists = true
+			}
+		}
+		array_equals = append(array_equals, exists)
+	}
+	assert.Equal(array_equals, []bool{true, true, true, true})
 }
