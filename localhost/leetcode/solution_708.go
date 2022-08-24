f11fc39b10ba6a97fddec395bf9b4d8f24d8e7eb
Evgenii Seliavka <eseliavka@twitter.com>
2022-08-24 11:30:50 -0400

    assigment #708

---
+++ localhost/leetcode/solution_708.go
@@ -0,0 +1,37 @@
+package solutions
+
+func insert(aNode *ListNode, x int) *ListNode {
+	if aNode == nil {
+		node := ListNode{Val: x, Next: nil}
+		node.Next = &node
+
+		return &node
+	}
+
+	curr := aNode.Next
+	prev := aNode
+
+	done := false
+
+	for curr != aNode {
+		if x >= prev.Val && x <= curr.Val {
+			done = true
+		} else if prev.Val > curr.Val {
+			if x >= prev.Val || x <= curr.Val {
+				done = true
+			}
+		}
+
+		if done {
+			prev.Next = &ListNode{Val: x, Next: curr}
+			return aNode
+		}
+
+		prev = curr
+		curr = curr.Next
+	}
+
+	prev.Next = &ListNode{Val: x, Next: curr}
+
+	return aNode
+}
--- localhost/leetcode/solutions_test.go
+++ localhost/leetcode/solutions_test.go
@@ -351,3 +351,27 @@ func TestSolution227(t *testing.T) {
 	res := calculate("14-3/2")
 	assert.Equal(res, 13, "Solution227")
 }
+
+func TestSolution708(t *testing.T) {
+	assert := assert.New(t)
+	head := ListNode{Val: 3, Next: nil}
+	head.Next = &ListNode{Val: 4, Next: nil}
+	head.Next.Next = &ListNode{Val: 1, Next: nil}
+	head.Next.Next.Next = &head
+
+	res := insert(&head, 2)
+	acc := make([]int, 0)
+
+	curr := res.Next
+	prev := res
+
+	for curr != res {
+		acc = append(acc, prev.Val)
+		prev = curr
+		curr = curr.Next
+	}
+
+	acc = append(acc, prev.Val)
+
+	assert.Equal(acc, []int{3, 4, 1, 2}, "Solution708")
+}
