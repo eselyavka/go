a62733c21a46dd6d9fbd5c6b401a84a3386018f5
Evgenii Seliavka <eseliavka@twitter.com>
2022-11-16 11:03:01 -0500

    assigment #100

---
+++ localhost/leetcode/solution_100.go
@@ -0,0 +1,17 @@
+package solutions
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+
+	if p != nil && q == nil {
+		return false
+	}
+
+	if p == nil && q != nil {
+		return false
+	}
+
+	return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
--- localhost/leetcode/solutions_test.go
+++ localhost/leetcode/solutions_test.go
@@ -682,3 +682,20 @@ func TestSolution104(t *testing.T) {
 	assert.Equal(3, actual, "Solution104")
 
 }
+
+func TestSolution100(t *testing.T) {
+	assert := assert.New(t)
+
+	root1 := TreeNode{Val: 1, Left: nil, Right: nil}
+	root1.Left = &TreeNode{Val: 2, Left: nil, Right: nil}
+	root1.Right = &TreeNode{Val: 3, Left: nil, Right: nil}
+
+	root2 := TreeNode{Val: 1, Left: nil, Right: nil}
+	root2.Left = &TreeNode{Val: 2, Left: nil, Right: nil}
+	root2.Right = &TreeNode{Val: 3, Left: nil, Right: nil}
+
+	actual := isSameTree(&root1, &root2)
+
+	assert.True(actual, "Solution100")
+
+}
