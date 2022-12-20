dc003bfb10824093f9d1e7e3ef9eee0862d3819b
Evgenii Seliavka <evg.selyavka@gmail.com>
2022-12-20 14:30:22 -0500

    refactor #53

--- localhost/leetcode/solution_53.go
+++ localhost/leetcode/solution_53.go
@@ -1,17 +1,13 @@
 package solutions
 
-import "math"
-
 func maxSubArray(nums []int) int {
-	if len(nums) == 1 {
-		return nums[0]
-	}
-	max_ending_here := 0
-	max_so_far := math.MinInt64
+	ans := MinInt
+	acc := 0
+
 	for _, num := range nums {
-		max_ending_here += num
-		max_so_far = MaxInts([]int{max_so_far, max_ending_here})
-		max_ending_here = MaxInts([]int{max_ending_here, 0})
+		acc += num
+		ans = MaxInts([]int{ans, acc})
+		acc = MaxInts([]int{acc, 0})
 	}
-	return int(max_so_far)
+	return ans
 }
