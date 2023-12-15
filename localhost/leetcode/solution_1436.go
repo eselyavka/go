8ff2145d702acd1b38471d8e368dc0263bbd9027
Evgenii Seliavka <evg.selyavka@gmail.com>
2023-12-15 12:17:29 -0500

    refactor #1436

--- localhost/leetcode/solution_1436.go
+++ localhost/leetcode/solution_1436.go
@@ -1,26 +1,5 @@
 package solutions
 
-/*
-class Solution(object):
-
-	def destCity(self, paths):
-	    """
-	    :type paths: List[List[str]]
-	    :rtype: str
-	    """
-	    G = {}
-
-	    for start, end in paths:
-	        G[start] = end
-
-	    ans = paths[0][0]
-
-	    while True:
-	        if ans in G:
-	            ans = G[ans]
-	            continue
-	        return ans
-*/
 func destCity(paths [][]string) string {
 	G := make(map[string]string)
 
