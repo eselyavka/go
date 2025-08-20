f6c0b0388422163771aca65f048109f9e154d44a
Evgenii Seliavka <evg.selyavka@gmail.com>
2025-08-20 16:59:48 -0400

    fix #1277

--- localhost/leetcode/solution_1277.go
+++ localhost/leetcode/solution_1277.go
@@ -4,16 +4,15 @@ func countSquares(matrix [][]int) int {
 	row := len(matrix)
 	col := len(matrix[0])
 
-	dp := make([][]int, row)
+	dp := make([][]int, row+1)
 	ans := 0
-	for i := range matrix {
-		dp[i] = make([]int, col)
-		copy(dp[i], matrix[i])
+	for i := 0; i < row+1; i++ {
+		dp[i] = make([]int, col+1)
 	}
 
-	for i := 1; i < row; i++ {
-		for j := 1; j < col; j++ {
-			if matrix[i][j] == 1 {
+	for i := 1; i < row+1; i++ {
+		for j := 1; j < col+1; j++ {
+			if matrix[i-1][j-1] == 1 {
 				dp[i][j] = 1 + MinInts([]int{dp[i-1][j], dp[i][j-1], dp[i-1][j-1]})
 				ans += dp[i][j]
 			}
