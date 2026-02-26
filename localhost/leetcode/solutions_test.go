2dd2fd3f6489afb86692098da4db406d095ce76b
Evgenii Seliavka <evg.selyavka@gmail.com>
2026-02-26 15:06:44 -0500

    assigment #2275

--- localhost/leetcode/solutions_test.go
+++ localhost/leetcode/solutions_test.go
@@ -2571,3 +2571,9 @@ func TestSolution3453(t *testing.T) {
 	actual := separateSquares([][]int{{0, 0, 1}, {2, 2, 1}})
 	assert.Equal(actual, 1.0, "SolutionOther3453")
 }
+
+func TestSolution2275(t *testing.T) {
+	assert := assert.New(t)
+	actual := largestCombination([]int{16, 17, 71, 62, 12, 24, 14})
+	assert.Equal(actual, 4, "SolutionOther2275")
+}
