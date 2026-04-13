206a5f270f8b0fdeb97ede398f7d48defa3f08d1
Evgenii Seliavka <evg.selyavka@gmail.com>
2026-04-13 12:29:51 -0400

    Remove unused imports from LeetCode 1848 test

--- problems/1801-2100/p1848/solution_1848_test.go
+++ problems/1801-2100/p1848/solution_1848_test.go
@@ -1,16 +1,9 @@
 package p1848
 
 import (
-	"sort"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-)
-
-var (
-	_ = sort.Strings
-	_ = strings.Join
 )
 
 func TestSolution1848(t *testing.T) {
