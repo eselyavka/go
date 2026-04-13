9396ebc260edacb48cc0d18034c6c9c18dc17cfa
Evgenii Seliavka <evg.selyavka@gmail.com>
2026-04-13 12:32:11 -0400

    Add LeetCode 1848 solution (#9)
    * Add LeetCode 1848 solution
    
    * Remove unused imports from LeetCode 1848 test

---
+++ problems/1801-2100/p1848/solution_1848_test.go
@@ -0,0 +1,13 @@
+package p1848
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolution1848(t *testing.T) {
+	assert := assert.New(t)
+	actual := getMinDistance([]int{1, 2, 3, 4, 5}, 5, 3)
+	assert.Equal(actual, 1, "Solution1848")
+}
