7b3b952a6d8f3cc1357c0af5c0753f061793fb7a
Evgenii Seliavka <evg.selyavka@gmail.com>
2026-05-23 15:29:03 -0400

    refactor number of islands dfs (#16)

--- problems/0001-0300/p200/solution_200.go
+++ problems/0001-0300/p200/solution_200.go
@@ -8,34 +8,35 @@ func numIslands(grid [][]byte) int {
 		seen[i] = make([]bool, n)
 	}
 
+	var dfs200 func(x, y int) int
+	dfs200 = func(x, y int) int {
+		if x >= m || x < 0 || y >= n || y < 0 {
+			return 0
+		}
+
+		if seen[x][y] {
+			return 0
+		}
+
+		seen[x][y] = true
+
+		if string(grid[x][y]) == "0" {
+			return 0
+		}
+
+		return 1 + dfs200(x-1, y) + dfs200(x+1, y) + dfs200(x, y-1) + dfs200(x, y+1)
+	}
+
 	res := 0
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
 			if seen[i][j] {
 				continue
 			}
-			if dfs200(grid, seen, m, n, i, j) > 0 {
+			if dfs200(i, j) > 0 {
 				res++
 			}
 		}
 	}
 	return res
-}
-
-func dfs200(grid [][]byte, seen [][]bool, m, n, x, y int) int {
-	if x >= m || x < 0 || y >= n || y < 0 {
-		return 0
-	}
-
-	if seen[x][y] {
-		return 0
-	}
-
-	seen[x][y] = true
-
-	if string(grid[x][y]) == "0" {
-		return 0
-	}
-
-	return 1 + dfs200(grid, seen, m, n, x-1, y) + dfs200(grid, seen, m, n, x+1, y) + dfs200(grid, seen, m, n, x, y-1) + dfs200(grid, seen, m, n, x, y+1)
 }
